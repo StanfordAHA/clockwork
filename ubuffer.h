6cd8e35cf97847791ec709d37a584a9e40ef189b
Maxwell Strange <mstrange@stanford.edu>
2025-03-01 12:55:39 -0800

    put Instance map behind compiler guards

--- ubuffer.h
+++ ubuffer.h
@@ -1115,9 +1115,9 @@ class UBuffer {
     map<string, vector<pair<int, int>>> precursor_committed;
     map<string, int> extra_data_locations;
 
-    map<string, CoreIR::Instance *> instance_map;
 
 #ifdef COREIR
+    map<string, CoreIR::Instance *> instance_map;
     json config_file;
 #endif
 
